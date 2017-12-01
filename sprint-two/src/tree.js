0f6fe506e7d87c85a10ed14dd5a5fde43f19439e
archana-nayak <archana.nayak@gmail.com>
2017-11-30 17:09:20 -0800

    tree is done

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,23 +1,47 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];// fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
+
 };
 
 treeMethods.contains = function(target) {
+  //var tree = null || this.
+  if (this.value === target) {
+    console.dir(this.value);
+    return true;
+  } 
+
+   
+  for (var i = 0; i < this.children.length; i++) {
+    var containsTarget = this.children[i].contains(target);
+    if (containsTarget) {
+      return true;
+    }
+  } 
+  return false;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
- */
+   Big O of addChild is logarithmic ie it traverses at the max the
+   height of the tree(from the root node to the leaf).
+  
+   Big O of contains is linear o(n) beacuse it might require us to visit
+   all of the children before we find the target
+
+ */ 
+
