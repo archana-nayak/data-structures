db9cbef1b619800bfe2b06bc8f51567ef82f9975
archana <archana.nayak@gmail.com>
2017-12-02 15:34:29 -0800

    "Add functionality for depthFirstLog and time complexity"

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -51,11 +51,25 @@ searchTreeMethods.contains = function(value) {
    return false;
 };
 
-searchTreeMethods.depthFirstLog = function(value) { 
-  
+searchTreeMethods.depthFirstLog = function(cb) { 
+  console.dir(this);
+  var current =  Array.prototype.slice.call(arguments)[1] || this ;
+  if (current) {
+    cb(current.value);	
+  } 
+  if (current.left) {
+    current.left.depthFirstLog(cb);
+  }
+  if (current.right) {
+    current.right.depthFirstLog(cb);
+  }
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ The time complexity of the contains is logarithmic because any given level, the number of nodes to
+ be searched is slashed in half. o(logn).
+ The time complexity of insert is logarithmic too because of the exact same reason as above. o(logn).
+ The time complexity of depthFirstLog is o(n) or linear because it has to visit all the nodes
  */
